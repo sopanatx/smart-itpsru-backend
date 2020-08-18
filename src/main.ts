671731e0cdf2b675f816765c41c1d4dfcbdb56cd
Sopanat <sopanus2014@gmail.com>
2020-08-19 01:22:24 +0700

    ✨Initial Swagger OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder()
+    .setTitle('SmartITPSRU')
+    .setDescription('ITPSRU API For Mobile Application.')
+    .setVersion('1.2')
+    .addTag('smart-itpsru')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
   await app.listen(7000);
 }
 bootstrap();
